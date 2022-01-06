c5e579ec9f3b0516538bdb8496de7c8720997ad2
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-01-06 18:20:05 +0100

    Mise à jour partielle de la page d'accueil

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,6 +7,12 @@ class Home extends React.Component {
         <div>
             <img className="banner_Img" src={Banner_Home} alt="Bannière"/>
             <div className="banner_Bgr"></div>
+            <div><span className="banner_Txt">Chez vous, partout et ailleurs</span></div>
+            <div className="gallery">
+                <div className="card"><div className="card_Title">Titre de la<br/>location</div></div>
+                <div className="card"><div className="card_Title">Titre de la<br/>location</div></div>
+                <div className="card"><div className="card_Title">Titre de la<br/>location</div></div>
+            </div>
         </div>)
     }
 }
