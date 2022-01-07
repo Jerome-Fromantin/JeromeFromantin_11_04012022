34e23d595df16505c019ecf323836b2471a03e79
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-01-07 18:05:00 +0100

    Quasi-finition de la page d'accueil

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import Banner_Home from '../assets/Banner_Home.png'
 
 class Home extends React.Component {
     render() {
         return (
         <div>
-            <img className="banner_Img" src={Banner_Home} alt="Bannière"/>
-            <div className="banner_Bgr"></div>
-            <div><span className="banner_Txt">Chez vous, partout et ailleurs</span></div>
+            <div className="banner">
+                <div className="banner_bgr"></div>
+                <div className="banner_txt">
+                    <span>Chez vous,&nbsp;</span>
+                    <span>partout et ailleurs</span>
+                </div>
+            </div>
             <div className="gallery">
-                <div className="card"><div className="card_Title">Titre de la<br/>location</div></div>
-                <div className="card"><div className="card_Title">Titre de la<br/>location</div></div>
-                <div className="card"><div className="card_Title">Titre de la<br/>location</div></div>
+                <div className="card"><div className="card_title">Titre de la<br/>location</div></div>
             </div>
         </div>)
     }
--- src/pages/layout.js
+++ src/pages/layout.js
@@ -6,11 +6,11 @@ import Footer from '../composants/Footer'
 class Layout extends React.Component {
     render() {
         return (<div>
-            <div className="layout_Main">
+            <div className="layout_main">
                 <Header/>
                 <Outlet/>
             </div>
-            <div className="layout_Footer">
+            <div className="layout_footer">
                 <Footer/>
             </div>
         </div>)
