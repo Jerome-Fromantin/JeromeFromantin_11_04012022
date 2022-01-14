723114fb0ae102c4dff9c636a9606cfb61f89c11
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-01-14 18:42:16 +0100

    Page << A propos >> : Travail sur les transitions et l'apparence mobile

--- src/pages/apropos.js
+++ src/pages/apropos.js
@@ -1,6 +1,41 @@
 import React from 'react';
+import down_arrow from '../assets/DownArrow.png'
+import up_arrow from '../assets/UpArrow.png'
 
 class Apropos extends React.Component {
+    openClose1() {
+        let arrow1 = document.getElementById("arrowA1")
+        arrow1.classList.toggle("downarrow_no")
+        let arrow2 = document.getElementById("arrowA2")
+        arrow2.classList.toggle("uparrow_yes")
+        let text = document.getElementById("texteA")
+        text.classList.toggle("texte_apropos_open")
+    }
+    openClose2() {
+        let arrow1 = document.getElementById("arrowB1")
+        arrow1.classList.toggle("downarrow_no")
+        let arrow2 = document.getElementById("arrowB2")
+        arrow2.classList.toggle("uparrow_yes")
+        let text = document.getElementById("texteB")
+        text.classList.toggle("texte_apropos_open")
+    }
+    openClose3() {
+        let arrow1 = document.getElementById("arrowC1")
+        arrow1.classList.toggle("downarrow_no")
+        let arrow2 = document.getElementById("arrowC2")
+        arrow2.classList.toggle("uparrow_yes")
+        let text = document.getElementById("texteC")
+        text.classList.toggle("texte_apropos_open")
+    }
+    openClose4() {
+        let arrow1 = document.getElementById("arrowD1")
+        arrow1.classList.toggle("downarrow_no")
+        let arrow2 = document.getElementById("arrowD2")
+        arrow2.classList.toggle("uparrow_yes")
+        let text = document.getElementById("texteD")
+        text.classList.toggle("texte_apropos_open")
+    }
+
     render() {
         return (
             <div>
@@ -8,23 +43,47 @@ class Apropos extends React.Component {
                     <div className="banner_bgr_apropos"></div>
                 </div>
                 <div className="divs_apropos">
-                    <div className="titre_apropos">Fiabilité</div>
-                    <div className="texte_apropos">
+                    <div className="titre_apropos" onClick={this.openClose1}>
+                        <span>Fiabilité</span>
+                        <span>
+                            <span id="arrowA1" className="downarrow"><img src={down_arrow} alt="Flèche vers le bas"/></span>
+                            <span id="arrowA2" className="uparrow"><img src={up_arrow} alt="Flèche vers le haut"/></span>
+                        </span>
+                    </div>
+                    <div id="texteA" className="texte_apropos">
                         Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes
                         aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
                     </div>
-                    <div className="titre_apropos">Respect</div>
-                    <div className="texte_apropos">
+                    <div className="titre_apropos" onClick={this.openClose2}>
+                        <span>Respect</span>
+                        <span>
+                            <span id="arrowB1" className="downarrow"><img src={down_arrow} alt="Flèche vers le bas"/></span>
+                            <span id="arrowB2" className="uparrow"><img src={up_arrow} alt="Flèche vers le haut"/></span>
+                        </span>
+                    </div>
+                    <div id="texteB" className="texte_apropos">
                         La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement
                         discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.
                     </div>
-                    <div className="titre_apropos">Service</div>
-                    <div className="texte_apropos">
+                    <div className="titre_apropos" onClick={this.openClose3}>
+                        <span>Service</span>
+                        <span>
+                            <span id="arrowC1" className="downarrow"><img src={down_arrow} alt="Flèche vers le bas"/></span>
+                            <span id="arrowC2" className="uparrow"><img src={up_arrow} alt="Flèche vers le haut"/></span>
+                        </span>
+                    </div>
+                    <div id="texteC" className="texte_apropos">
                         Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite.
                         N'hésitez pas à nous contacter si vous avez la moindre question.
                     </div>
-                    <div className="titre_apropos">Sécurité</div>
-                    <div className="texte_apropos">
+                    <div className="titre_apropos" onClick={this.openClose4}>
+                        <span>Sécurité</span>
+                        <span>
+                            <span id="arrowD1" className="downarrow"><img src={down_arrow} alt="Flèche vers le bas"/></span>
+                            <span id="arrowD2" className="uparrow"><img src={up_arrow} alt="Flèche vers le haut"/></span>
+                        </span>
+                    </div>
+                    <div id="texteD" className="texte_apropos">
                         La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque
                         logement correspond aux critères de sécurité établis par nos services. En laissant une note
                         aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards
