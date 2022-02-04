50b63daa6f8979339450bcd4a7bd4ef0f1d8061c
Jérôme Fromantin <jeromefromantin@hotmail.fr>
2022-02-04 19:29:49 +0100

    Amélioration de l'apparence des cartes

--- src/pages/home.js
+++ src/pages/home.js
@@ -28,8 +28,8 @@ function Home(props) {
                     <li key={id} className="card">
                         <Link to={"logement/" + id}>
                             <div><img src={cover} alt="Logement" className="card_photo"/></div>
-                            <div className="card_title">{title}</div>
                             <div className="card_bgr"></div>
+                            <div className="card_title">{title}</div>
                         </Link>
                     </li>
                 ))}
